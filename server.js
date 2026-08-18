b13ed52e6bdbd9b09904d4b0934a7b1117a38b9e
Khushi Gupta <khushigupta512005@gmail.com>
2026-08-18 09:44:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -81,5 +81,5 @@ app.use((err, req, res, next) => {
 // ==========================================
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
